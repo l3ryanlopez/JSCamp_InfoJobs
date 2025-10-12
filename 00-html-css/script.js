0cfc47fcbdf61cfdf1cbe611bed47cd7cbc7b8d2
BryanLopez <bryanlopez09@outlook.es>
2025-10-12 17:58:27 -0600

    Se actualizan estilos

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,5 +1,41 @@
-const boton = document.querySelector("#boton-importante");
+// recupera solo el primer boton que encuentre
+// const boton = document.querySelector('.button-apply-job')
+// console.log(boton) // null si no lo encuentra
 
-boton.addEventListener("click", () => {
-  alert("Gracias por la visita");
-});
+// if (boton !== null) {
+//   boton.addEventListener('click', function() {
+//     boton.textContent = '¡Aplicado!'
+//     boton.classList.add('is-applied')
+//     boton.disabled = true
+//   })
+// }
+
+// const botones = document.querySelectorAll('.button-apply-job')
+// // devuelve un NodeList (array-like) con todos los botones que encuentre
+// // o una lista vacia [] si no encuentra ninguno
+
+// botones.forEach(boton => {
+//   boton.addEventListener('click', function() {
+//     boton.textContent = '¡Aplicado!'
+//     boton.classList.add('is-applied')
+//     boton.disabled = true
+//   })
+// })
+
+const jobsListingSection = document.querySelector('.jobs-listings')
+
+jobsListingSection.addEventListener('click', function(event) {
+  const element = event.target
+
+  if (element.classList.contains('button-apply-job')) {
+    element.textContent = '¡Aplicado!'
+    element.classList.add('is-applied')
+    element.disabled = true
+  }
+})
+
+const filter = document.querySelector('#filter-technology')
+
+filter.addEventListener('change', function () {
+  console.log(filter.value)
+})
